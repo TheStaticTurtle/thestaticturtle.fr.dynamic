62c3295e5e91ed44ca269d8402e9ab2d1caeb8bf
TheStaticTurtle <tuglersamuel@gmail.com>
2021-07-26 17:22:04 +0200

    Trying to deploy correctly

--- thestaticturtle/settings.py
+++ thestaticturtle/settings.py
@@ -49,7 +49,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website.apps.WebsiteConfig'
+    'website.apps.WebsiteConfig',
+    'mod_wsgi.server',
 ]
 
 MIDDLEWARE = [
